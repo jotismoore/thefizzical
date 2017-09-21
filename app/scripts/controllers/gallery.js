06dbb6576b95a2fe42209522f639d6f62dd6a790
Jotis Moore <jmoore@urbanoutfitters.com>
2017-09-21 20:25:29 +0100

    further updates

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -10,13 +10,9 @@
 angular.module('fizzyFlutesApp')
   .controller('GalleryCtrl', function($scope, instagram) {
     instagram.fetchTagged(function(data) {
-      $scope.thumbnailSize = 8;
-      $scope.thumbnailPage = 1;
       var slides = $scope.slides = data;
-      $scope.showThumbnails = slides.slice(($scope.thumbnailPage-1)*$scope.thumbnailSize,$scope.thumbnailPage*$scope.thumbnailSize);
       $scope.setActive = function(idx) {
-        $scope.slides[idx].active=true;
-        $scope.showThumbnails[idx].active=true;
+        slides[idx].active=true;
       }
     });
   })
@@ -29,7 +25,7 @@ angular.module('fizzyFlutesApp')
             userId: '209854882',
             accessToken: '209854882.ba4c844.ae48160551b548229840b5827b2a1574',
             sortBy: 'most-recent',
-            limit:'8',
+            limit:'5',
             success: function(response) {
               callback(response.data);
             }
